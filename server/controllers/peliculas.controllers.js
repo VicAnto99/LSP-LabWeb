6a6c5420b716120ad3c42e2d61af25da8f6a46f1
VicAnto <A01339529@itesm.mx>
2021-09-16 04:44:45 -0500

    RES API ya está creada
    :)

--- server/controllers/peliculas.controllers.js
+++ server/controllers/peliculas.controllers.js
@@ -1,20 +1,43 @@
 const Pelicula = require('../models/pelicula');
 const peliculasCtrl = { };
 
-peliculasCtrl.getPeliculas = (req, res) => {
-    Pelicula.find();
-}
-peliculasCtrl.createPeliculas = function (){
-
-}
-peliculasCtrl.getPelicula = function (){
-
-}
-peliculasCtrl.editPelicula = function(){
-
-}
-peliculasCtrl.deletePelicula = function(){
-
-}
+peliculasCtrl.getPeliculas = async (req, res) => {
+    const peliculas = await Pelicula.find();
+    res.json({
+        mensaje:"Peliculas mostradas",
+        peliculas: peliculas});
+};
+peliculasCtrl.createPeliculas = async (req, res) => {
+    const pelicula = new Pelicula(req.body);
+    await pelicula.save();
+    res.json({
+        mensaje:"Pelicula creada",
+        pelicula: pelicula});
+};
+peliculasCtrl.getPelicula = async (req, res) => {
+    const pelicula = await Pelicula.findById(req.body.id);
+    res.json({
+        mensaje:"Pelicula encontrada",
+        pelicula: pelicula});
+};
+peliculasCtrl.editPelicula = async (req, res) => {
+    const {id} = req.params;
+    const pelicula = {
+        tipo: req.body.tipo,
+        medida: req.body.medida,
+        provedor: req.body.provedor,
+        unidades: req.body.unidades
+    }
+    await Pelicula.findByIdAndUpdate(id, {$set: pelicula}, {new: true});
+    res.json({
+        mensaje:"Pelicula actualizada",
+        pelicula: pelicula});
+};
+peliculasCtrl.deletePelicula = async (req, res) => {
+    const {id} = req.params;
+    await Pelicula.findByIdAndRemove(id);
+    res.json({
+        mensaje:"Pelicula eliminada"});
+};
 
 module.exports = peliculasCtrl;
